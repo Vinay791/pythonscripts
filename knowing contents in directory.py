7fb8bcf6dd47f795f129bf36d2ac64a58e5f6c2a
vinay <reddyvinay93816@gmail.com>
2023-04-22 14:01:22 +0530

    second commit

---
+++ knowing contents in directory.py
@@ -0,0 +1,10 @@
+#python script for knowing the contents in a file or directory 
+
+import os
+dir = " "  # In dir variable we have to assign directory inside the double quotes
+for name in os.listdir(dir):
+  fullname = os.path.join(dir, name)
+  if os.path.isdir(fullname):
+    print("{} is a directory".format(fullname))
+  else:
+    print("{} is a file".format(fullname))
