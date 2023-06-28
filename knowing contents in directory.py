c7a519d095b8ab3a33c8069c34fcc37a0d127908
vinay <reddyvinay93816@gmail.com>
2023-06-28 13:39:25 +0530

    third commit

--- knowing contents in directory.py
+++ knowing contents in directory.py
@@ -1,7 +1,7 @@
 #python script for knowing the contents in a file or directory 
 
 import os
-dir = " "  # In dir variable we have to assign directory inside the double quotes
+dir = "C:\devops project"  # In dir variable we have to assign directory inside the double quotes
 for name in os.listdir(dir):
   fullname = os.path.join(dir, name)
   if os.path.isdir(fullname):
